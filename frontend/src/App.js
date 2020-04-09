92f24898441af3e7d9182e8f68765e41bbfd3a5c
Claudiu C. Bogdan <claudiu@mindtreatstudios.com>
2020-04-09 16:39:51 +0300

    Reformat code. Add db connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
-
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 // import logo from './logo.svg';
 import './stylesheets/App.css';
 import FormView from './components/FormView';
@@ -14,22 +9,22 @@ import QuizView from './components/QuizView';
 
 
 class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <Header path />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={QuestionView} />
-          <Route path="/add" component={FormView} />
-          <Route path="/play" component={QuizView} />
-          <Route component={QuestionView} />
-        </Switch>
-      </Router>
-    </div>
-  );
+    render() {
+        return (
+            <div className="App">
+                <Header path/>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={QuestionView}/>
+                        <Route path="/add" component={FormView}/>
+                        <Route path="/play" component={QuizView}/>
+                        <Route component={QuestionView}/>
+                    </Switch>
+                </Router>
+            </div>
+        );
 
-  }
+    }
 }
 
 export default App;
